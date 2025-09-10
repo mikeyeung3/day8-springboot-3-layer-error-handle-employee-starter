01570e65cc0dc41f1bc3061f17baf7614d7bf31f
Mike Yeung <yeungmi3@oocl.com>
2025-09-10 10:18:31 +0800

    refactor：should return all employee

--- src/main/java/com/example/demo/Employee.java
+++ src/main/java/com/example/demo/Employee.java
@@ -13,6 +13,15 @@ public class Employee {
     private Integer age;
     private String gender;
     private Double salary;
+
+    public Employee(Integer id, String name, Integer age, String gender, Double salary) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.salary = salary;
+    }
+
     public Integer getId() {
         return id;
     }
