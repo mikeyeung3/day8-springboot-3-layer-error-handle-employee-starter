adc0882d1d9bf92563a6e3f3cbfefde3f76cd3d0
Mike Yeung <yeungmi3@oocl.com>
2025-09-10 16:34:55 +0800

    feat: exception handler

---
+++ src/main/java/com/example/demo/controller/advice/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller.advice;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResponseStatusException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseException exceptionHandler(Exception e) {
+        return new ResponseException(e.getMessage());
+    }
+}
