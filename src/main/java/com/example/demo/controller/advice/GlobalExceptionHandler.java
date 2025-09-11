1e3647276801bff3452e15771a12fa706470b4b2
Mike Yeung <yeungmi3@oocl.com>
2025-09-11 16:25:22 +0800

    feat: exception handler for argument not valid

--- src/main/java/com/example/demo/controller/advice/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/controller/advice/GlobalExceptionHandler.java
@@ -3,11 +3,15 @@ package com.example.demo.controller.advice;
 import com.example.demo.exception.InvaildSalaryForEmployeeAgeGreaterThan29Exception;
 import com.example.demo.exception.InvalidAgeEmployeeException;
 import com.example.demo.exception.InvalidOperationOnInactiveEmployeeException;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -23,6 +27,16 @@ public class GlobalExceptionHandler {
     public ResponseException invalidAgeEmployeeExceptionHandler(Exception e) {
         return new ResponseException(e.getMessage());
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseException handlerArgumentNotValid(MethodArgumentNotValidException exception){
+        String errorMessage = exception.getBindingResult().getFieldErrors().stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.joining(" | "));
+
+        return new ResponseException(errorMessage);
+    }
+
 
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
